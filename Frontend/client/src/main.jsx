9048ac5fdfa5747ae7e70a37cfb3b8a1f5d508a7
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2026-02-24 09:16:20 +0530

    Update main.jsx

--- Frontend/client/src/main.jsx
+++ Frontend/client/src/main.jsx
@@ -9,7 +9,7 @@ import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="294951079333-ctcqe6c2fns2klrd5qoebahbjm9rm3kr.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="nokey">
       <BrowserRouter>
       <Provider store={store}>
         <App />
