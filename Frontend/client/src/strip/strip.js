e25c28710d9d47e4c2a939e0dc0e4d565d72afb1
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2026-02-24 09:15:38 +0530

    Update strip.js

--- Frontend/client/src/strip/strip.js
+++ Frontend/client/src/strip/strip.js
@@ -1,3 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe("pk_test_51SiV7ARwGOstWmy6wtO9hbdER8gkHXz1tfiYEetyVQbXpdl9wXtY3W4G1duB9CVsEFYMrq2hkSphQwfjL10YZxjv00dj3S6hLN");
+export const stripePromise = loadStripe("no key");
