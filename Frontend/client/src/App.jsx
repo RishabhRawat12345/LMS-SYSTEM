8ba6c5856321940886d8a41e474dcda1f1f9c5ed
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2026-02-24 12:09:38 +0530

    Update App.jsx

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -3,13 +3,12 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
-
 import Signup from "./components/Signup/Signup";
 import Signin from "./components/Signin/Signin";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import Course from "./components/Educator/Course";
-import Forgetpass from "./components/passwordreset/Forgetpass";
+import Forgetpass from "./components/passwordreset/forgetpass";
 import Otp from "./components/passwordreset/Otp";
 import Resetpass from "./components/passwordreset/Resetpass";
 import Role from "./components/role/Role";
@@ -18,14 +17,15 @@ import CreateCourse from "./components/Educator/CreateCourse";
 import EditCourse from "./components/Educator/EditCourse";
 import ViewAllcourses from "./components/Educator/ViewAllcourses";
 import CreateLecture from "./components/Educator/CreateLecture";
-import EditLecture from "./components/Educator/EditLecture";
-import AboutCourse from "./components/Educator/AboutCourse";
 import Nav from "./components/nav/Nav";
-import Cancel from "../Success/Cancel";
-import PaymentSuccess from "../Success/Success";
 
 import useUserHook from "./hooks/userhooks";
 import usecoursehooks from "./hooks/usecoursehooks";
+import { getcourse } from "./redux/courseSlice";
+import EditLecture from "./components/Educator/EditLecture";
+import AboutCourse from "./components/Educator/AboutCourse";
+import Cancel from "../Success/Cancel";
+import PaymentSuccess from "../Success/Success";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -41,43 +41,29 @@ const App = () => {
       getUserById(userid);
     }
 
+    const saved = JSON.parse(localStorage.getItem("courses"));
+
+    if (saved) {
+      dispatch(getcourse(saved));
+    }
+
     fetchdata();
   }, []);
 
-  useEffect(() => {
-    if (location.pathname === "/courses") {
-      fetchdata();
-    }
-  }, [location.pathname]);
-
   const userdata = useSelector((state) => state.auth.user);
+  const courses = useSelector((state) => state.course.course);
 
-  
-  const courses = useSelector((state) => state.course.courses);
-
-  const hideNavRoutes = [
-    "/",
-    "/signin",
-    "/role",
-    "/editc",
-    "/courses",
-    "/createl",
-    "/aboutC",
-    "/viewc",
-  ];
-
+  const hideNavRoutes = ["/", "/signin", "/role",'/editc','/courses','/createl','/aboutC','/viewc'];
   const shouldShowNav = !hideNavRoutes.includes(location.pathname);
-
+ useEffect(()=>{
+  console.log("the course data in app",courses)
+  console.log("the userdata is in app",userdata)
+ })
   return (
     <>
       {shouldShowNav && <Nav />}
 
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        theme="colored"
-      />
-
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
       <Routes>
         <Route path="/" element={<Signup userdata={userdata} />} />
         <Route path="/signin" element={<Signin />} />
@@ -102,24 +88,15 @@ const App = () => {
         <Route path="/reset" element={<Resetpass />} />
         <Route path="/role" element={<Role />} />
 
-        <Route
-          path="/dash"
-          element={<Dashboard userdata={userdata} coursedata={courses} />}
-        />
-
+        <Route path="/dash" element={<Dashboard   userdata={userdata}   coursedata={courses} />} />
         <Route path="/createCourse" element={<CreateCourse />} />
         <Route path="/editc" element={<EditCourse />} />
         <Route path="/viewc" element={<ViewAllcourses />} />
         <Route path="/createl" element={<CreateLecture />} />
-        <Route path="/editl" element={<EditLecture />} />
-
-        <Route
-          path="/aboutC"
-          element={<AboutCourse userdata={userdata} />}
-        />
-
-        <Route path="/payment-success" element={<PaymentSuccess />} />
-        <Route path="/payment-cancel" element={<Cancel />} />
+        <Route path="/editl" element={<EditLecture/>}/>
+        <Route path="/aboutC"  element={<AboutCourse  userdata={userdata}/>}/>
+        <Route  path="/payment-success"element={<PaymentSuccess />}/>
+        <Route path="/payment-cancel" element={<Cancel/>}/>
       </Routes>
     </>
   );
