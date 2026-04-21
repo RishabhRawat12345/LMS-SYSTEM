c2bb181b3e64ca62f3a46570c72490c332aead95
Rishabh Rawat <150123998+RishabhRawat12345@users.noreply.github.com>
2026-04-21 15:59:16 +0530

    Update firebase.js

--- Frontend/client/utils/firebase.js
+++ Frontend/client/utils/firebase.js
@@ -13,14 +13,7 @@ const firebaseConfig = {
   messagingSenderId: "555800197112",
   appId: "1:555800197112:web:5a1e7d4dabd92438e7480d",
 };
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIRE_KEY,
-  authDomain: "lms-5477b.firebaseapp.com",
-  projectId: "lms-5477b",
-  storageBucket: "lms-5477b.firebasestorage.app",
-  messagingSenderId: "10309403513",
-  appId: "1:10309403513:web:3b03c18b9b049f8d92a5a8"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
